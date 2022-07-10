d212e283989c443d980dbc21774583106b6c6f32
Zhang Jiale <zhangjialecn@gmail.com>
2022-07-10 16:35:52 +0800

    15

--- 15.3-sum.rs
+++ 15.3-sum.rs
@@ -9,6 +9,50 @@ use std::collections::HashMap;
 impl Solution {
     pub fn three_sum(mut nums: Vec<i32>) -> Vec<Vec<i32>> {
         nums.sort();
+        let mut triplets: Vec<Vec<i32>> = vec![];
+        for i in 0..(nums.len() - 2) {
+            if nums[i] > 0 {
+                break;
+            }
+
+            if i > 0 && nums[i] == nums[i - 1] {
+                continue;
+            }
+
+            let (mut left, mut right) = (i + 1, nums.len() - 1);
+
+            while left < right {
+                let sum = nums[i] + nums[left] + nums[right];
+
+                if sum > 0 {
+                    right -= 1;
+                    while right > left && nums[right] == nums[right + 1] {
+                        right -= 1;
+                    }
+                } else if sum < 0 {
+                    left += 1;
+                    while left < right && nums[left] == nums[left - 1] {
+                        left += 1;
+                    }
+                } else {
+                    triplets.push(vec![nums[i], nums[left], nums[right]]);
+                    while right > left && nums[right - 1] == nums[right] {
+                        right -= 1;
+                    }
+                    while left < right && nums[left + 1] == nums[left] {
+                        left += 1;
+                    }
+                    right -= 1;
+                    left += 1;
+                }
+            }
+        }
+
+        triplets
+    }
+
+    pub fn three_sum_1(mut nums: Vec<i32>) -> Vec<Vec<i32>> {
+        nums.sort();
 
         let nums_indexes: HashMap<&i32, usize> = nums
             .iter()
