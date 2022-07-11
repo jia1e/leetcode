d48b7a4c367a9fd4c89df381b9828ce088fae2ba
Zhang Jiale <zhangjialecn@gmail.com>
2022-07-11 15:04:36 +0800

    225

--- 225.implement-stack-using-queues.rs
+++ 225.implement-stack-using-queues.rs
@@ -7,15 +7,77 @@
 // @lc code=start
 use std::collections::VecDeque;
 pub struct MyStack {
-    primary: VecDeque<i32>,
-    secondary: VecDeque<i32>,
+    queue: VecDeque<i32>,
 }
 
 /**
  * `&self` means the method takes an immutable reference.
  * If you need a mutable reference, change it to `&mut self` instead.
  */
+
 impl MyStack {
+    pub fn new() -> Self {
+        Self {
+            queue: VecDeque::new(),
+        }
+    }
+
+    pub fn push(&mut self, x: i32) {
+        self.queue.push_back(x);
+    }
+
+    pub fn pop(&mut self) -> i32 {
+        if self.queue.len() == 0 {
+            return 0;
+        }
+        let mut n = self.queue.len() - 1;
+        while n > 0 {
+            let front = self.queue.pop_front().unwrap();
+            self.queue.push_back(front);
+            n -= 1;
+        }
+        self.queue.pop_front().unwrap_or_default()
+    }
+
+    pub fn top(&mut self) -> i32 {
+        if self.queue.len() == 0 {
+            return 0;
+        }
+
+        let mut n = self.queue.len() - 1;
+        while n > 0 {
+            let front = self.queue.pop_front().unwrap();
+            self.queue.push_back(front);
+            n -= 1;
+        }
+
+        let top = self.queue.pop_front().unwrap();
+        self.queue.push_back(top);
+
+        top
+    }
+
+    pub fn empty(&self) -> bool {
+        self.queue.is_empty()
+    }
+}
+
+/**
+ * Your MyStack object will be instantiated and called as such:
+ * let obj = MyStack::new();
+ * obj.push(x);
+ * let ret_2: i32 = obj.pop();
+ * let ret_3: i32 = obj.top();
+ * let ret_4: bool = obj.empty();
+ */
+// @lc code=end
+
+pub struct MyStack2 {
+    primary: VecDeque<i32>,
+    secondary: VecDeque<i32>,
+}
+
+impl MyStack2 {
     pub fn new() -> Self {
         Self {
             primary: VecDeque::new(),
@@ -56,19 +118,10 @@ impl MyStack {
     }
 }
 
-/**
- * Your MyStack object will be instantiated and called as such:
- * let obj = MyStack::new();
- * obj.push(x);
- * let ret_2: i32 = obj.pop();
- * let ret_3: i32 = obj.top();
- * let ret_4: bool = obj.empty();
- */
-// @lc code=end
-
 #[test]
 fn test() {
     let mut obj = MyStack::new();
+    assert_eq!(obj.pop(), 0);
     assert_eq!(obj.empty(), true);
     obj.push(1);
     assert_eq!(obj.empty(), false);
